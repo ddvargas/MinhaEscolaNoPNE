08d357f4740b71c3f38ecc1775b0e76af51b9258
ddvargas <ddvargas@inf.ufsm.br>
2019-09-01 00:58:37 -0300

    Recuperar listas de escolas do estado selecionado.

--- scripts/index_scripts.js
+++ scripts/index_scripts.js
@@ -1,25 +1,52 @@
 var estados;
 var escolas;
-if(self.fetch) {
-    // execute minha solicitação do fetch
-    console.log("Iniciando busca de estados");
-    estados = fetch("https://biod.c3sl.ufpr.br/api/v1/data?dimensions=dim:estado:nome&metrics")
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {listarEstados(data)})   ;
-} else {
-    // faça solicitação de lista de estados por meio de XMLHttpRequest
-}
+
+window.onload = function () {
+    if(self.fetch) {
+        // execute minha solicitação do fetch
+        console.log("Iniciando busca de estados");
+        estados = fetch("https://biod.c3sl.ufpr.br/api/v1/data?dimensions=dim:estado:nome&metrics")
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {listarEstados(data)})   ;
+    } else {
+        // faça solicitação de lista de estados por meio de XMLHttpRequest
+    }
+};
 
 
 function listarEstados(estadosRetornados) {
     console.log(estadosRetornados);
     //testar o tamanho dos estados selecionados
     var estadosSelect  = document.getElementById("selecionar_estados");
-    for (var i = 0; i <= estadosRetornados.length; i++){
+    for (var i in estadosRetornados){
         var nome = estadosRetornados[i]["dim:estado:nome"];
-        var op = new Option(nome, nome);
+        var op = new Option(nome, nome, false, false);
         estadosSelect.add(op)
     }
+}
+
+
+function itemEstadoMudado() {
+    var sel = document.getElementById("selecionar_estados");
+    var itemSelecionado = sel.options[sel.selectedIndex];
+    if (self.fetch){
+        //realizar busca pelo fetch
+        var link = "https://biod.c3sl.ufpr.br/api/v1/data?metrics&dimensions=dim:escola:nome,dim:escola:id,dim:cidade:id&filters=dim:estado:nome==";
+        link += itemSelecionado.value.toString();
+        fetch(link)
+            .then(response => {return response.json();})
+            .then( data => {recuperarDados(data);});
+
+        console.log("buscou escolas");
+
+    }else{
+        //realizar busca por meio de XMLHttpRequest
+    }
+}
+
+function recuperarDados(retornados) {
+    escolas = retornados;
+    console.log(escolas);
 }
